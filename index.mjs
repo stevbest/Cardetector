79062e9f6f3e8d0265cee8700c77053d99be04f9
StevBest <35335757+stevbest@users.noreply.github.com>
2023-06-08 13:27:04 +0000

    Add files via upload

---
+++ index.mjs
@@ -0,0 +1,33 @@
+
+var card_container = document.getElementById('container');
+var card_no = document.getElementById('txtcardno');
+var card_sec_pin = document.getElementById('txtcardsecpin');
+
+
+
+var sgnt = document.getElementById('hiddenpin');
+var cardno = document.getElementById('cardnumber');
+var cardname = document.getElementById('cardinfo__name');
+
+var foundcard = () =>{
+    sgnt.innerHTML = card_sec_pin.value;
+    cardno.innerHTML = card_no.value;
+    cardname.innerHTML = 'MONDAY S. O.';
+}
+
+// click btn to check serie is equal or greater than 3
+document.getElementById('btndetect').onclick = () =>{
+    if(card_sec_pin.value.length > 3 ||
+         card_sec_pin.value.length < 3)
+    {
+        card_sec_pin.style.background = 'rgba(248, 0, 0, 0.5)';
+        card_container.style.display = "none";
+        // alert('check');
+    }
+    else{
+        card_container.style.display = "block";
+        card_sec_pin.style.background = 'rgba(255, 255, 255, 0.1)';
+        foundcard();
+
+    }
+};
