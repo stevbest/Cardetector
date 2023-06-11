2af816523b6349eb58d96e63da4237f310c667e7
StevBest <35335757+stevbest@users.noreply.github.com>
2023-06-11 17:58:54 +0000

    Add files via upload

--- index.mjs
+++ index.mjs
@@ -1,21 +1,37 @@
 
-// var card_container = document.getElementById('container');
-// var card_no = document.getElementById('txtcardno');
-// var card_sec_pin = document.getElementById('txtcardsecpin');
 
-// // document.getElementById('btndetect').addEventListener('click',()=>{
-// //     if(card_container.style.display == "none"){
-// //         card_container.style.display = "block";
-// //     }
-// //     else if(card_container.style.display == "block"){
-// //         card_container.style.display = "none";
-// //     }
-// // });|| card_sec_pin.innerHTML.length < 3
-// document.getElementById('btndetect').onclick = () =>{
-//     if(card_sec_pin.innerHTML.length > 3 || card_sec_pin.innerHTML.length < 3)
-//     {
-//         //card_sec_pin.style.background = 'rgba(248, 0, 0, 0.5)';
-//         // card_container.style.display = "none";
-//         alert('check');
-//     }
-// };
+// query all variables
+var cardarea = document.getElementById('card_display');
+var entryarea = document.getElementById('card_entry');
+
+
+
+var btndetect = document.getElementById('btn_check');
+
+//get card details
+var cardsecpin = document.getElementById('centre__secpin');
+var cardno = document.getElementById('centre__cardno');
+var cardname = document.getElementById('cardname');
+
+
+//hide card area
+cardarea.addEventListener('click', ()=>{
+    cardarea.style.display = 'none';
+    entryarea.style.display = 'flex';
+});
+
+
+//show card area if condition met
+btndetect.onclick = ()=>{
+    var txtcardno = document.getElementById('card_no');
+    var txtsecpin = document.getElementById('card_sec');
+    var inputValue = txtsecpin.value.trim();
+    if(inputValue.length == 3){
+
+        cardsecpin.innerHTML = txtsecpin.value;
+        cardno.innerHTML = txtcardno.value;
+        cardname.innerHTML = 'MONDAY S. O.';
+        cardarea.style.display = 'block';
+        entryarea.style.display = 'none';
+    }
+}
